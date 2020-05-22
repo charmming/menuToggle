164abd07cca4e3f54f6f36e9e41926c39a354f72
charmming <charmming4u@gmail.com>
2020-05-22 12:00:26 +0100

    commit all files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let logo = document.querySelector('.logo');
+let content = document.querySelector('.content');
+
+function display(){
+    if (content.style.display == 'none'){
+        content.style.display = 'block';
+    }else{
+        content.style.display = 'none';
+    }
+}
